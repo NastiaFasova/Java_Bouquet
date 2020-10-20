4616b300b7c5263110e0acd847a8d7fb8f722cdc
rukavi4kamax1 <rukavi4ka.max@gmail.com>
2020-10-20 20:54:56 +0300

    Added classes, enums and interfaces

---
+++ src/main/java/bouquet/Employee.java
@@ -0,0 +1,25 @@
+package bouquet;
+
+public abstract class Employee {
+    private String name;
+    private String surname;
+    private int experience;
+    private int age;
+
+    public Employee(String name, String surname, int experience, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.experience = experience;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", experience=" + experience +
+                ", age=" + age +
+                '}';
+    }
+}
